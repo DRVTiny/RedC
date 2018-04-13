5221d94389e5acf0934bfff947a649dde78fe361
Andrey A. Konovalov <drvtiny@gmail.com>
2018-04-13 20:53:26 +0300

    Replaced CBOR::XS with Tag::DeCoder

--- lib/RedC.pm
+++ lib/RedC.pm
@@ -1,17 +1,20 @@
 #!/usr/bin/perl
 package RedC;
-use parent 'Redis::Fast';
+use constant {
+    RECONNECT_UP_TO	=>	 10,		# Reconnect up to 10 seconds
+    RECONNECT_EVERY	=>	 100_000,	# Reconnect every 100 ms
+    DFLT_ENCODER_TAG	=> 	'CB',		# Ecnode complex structures to CBOR by default
+};
+
 use strict;
+use parent 'Redis::Fast';
 use utf8;
 binmode $_=>':utf8' for *STDOUT,*STDERR;
 use 5.16.1;
-use constant {
-    RECONNECT_UP_TO=>10,	# Reconnect up to 10 seconds
-    RECONNECT_EVERY=>100,	# Reconnect every 100 ms
-};
 
-use CBOR::XS;
+use Tag::DeCoder;
 use Carp qw(confess);
+use Ref::Util qw(is_hashref is_plain_coderef is_plain_hashref is_plain_arrayref);
 use Scalar::Util qw(refaddr);
 
 use Exporter qw(import);
@@ -19,103 +22,98 @@ our @EXPORT_OK=qw(get_redc_by);
 
 my (%conoByName, %conoByRef);
 # By default, after connection failure try to reconnect every 100 ms up to 10 seconds
-my $redTestObj=Redis::Fast->new('reconnect'=>RECONNECT_UP_TO,'every'=>RECONNECT_EVERY);
-my $cbor=CBOR::XS->new;
-
+my $redTestObj = Redis::Fast->new('reconnect'=>RECONNECT_UP_TO, 'every'=>RECONNECT_EVERY);
 sub new {
+    state $exclOptions={'on_connect'=>1, 'encoder'=>1};
     my ($class, %options)=@_;
     
-    my ($coName,$conIndex)=delete @options{qw/name index/};
+    my ($coName, $conIndex) = delete @options{qw/name index/};
     $conIndex=0 unless defined $conIndex and $conIndex!~/[^\d]/;
-    
+    $coName //= join('_' => 'redc', 'anon', 'pid'.$$, int(rand 65536));
     confess qq(Cant initialize RedC: name '$coName' was already reserved as Redis connector name)
         if $conoByName{$coName};
     
     if ($conIndex) {
-        my $nDB=databases($redTestObj);
-        confess sprintf('Redis database index #%s is out of configured bounds (min=0, max=%d)',$conIndex,$nDB-1)
+        my $nDB = databases($redTestObj);
+        confess sprintf('Redis database index #%s is out of configured bounds (min=0, max=%d)',$conIndex, $nDB-1)
             if $conIndex>=$nDB;
     }
     
-    my $redC=$class->SUPER::new(
+    my $redC = $class->SUPER::new(
 # Hint: you can redefine "reconnection" settings in your %options passed to constructor
-        'reconnect'=>RECONNECT_UP_TO,
-        'every'=>RECONNECT_EVERY,
-        $conIndex
-            ? ('on_connect'=>sub {
-                    $_[0]->SUPER::select($conIndex);
-              })
-            : (),
-        %options,
+        'reconnect'	=>	RECONNECT_UP_TO,
+        'every'		=>	RECONNECT_EVERY,
+        'on_connect'	=>	sub {
+            my $self = shift;
+            $options{'on_connect'} and is_plain_coderef($options{'on_connect'}) and $options{'on_connect'}->($self);
+            $self->SUPER::select($conIndex) if $conIndex;
+            $self->SUPER::client_setname(
+                (
+                    (defined($options{'client_name'}) and !ref $options{'client_name'})
+                        ? $options{'client_name'}
+                        : "${coName}_$$"
+                ) =~ s%\s%_%gr
+            );
+        },
+        (map {$_=>$options{$_}} grep { !$exclOptions->{$_} } keys %options),
     );
-    
-    $conoByRef{refaddr $redC}=$conoByName{$coName}={'name'=>$coName,'index'=>$conIndex,'redc'=>\$redC};
+    $conoByRef{refaddr $redC} = $conoByName{$coName} = {'name'=>$coName, 'index'=>$conIndex, 'redc'=>\$redC};
     return $redC;
+}
+
+sub encoder {
+    $_[1] ? $conoByRef{refaddr $_[0]}{'encoder_tag'} = $_[1] : $conoByRef{refaddr $_[0]}{'encoder_tag'} //= DFLT_ENCODER_TAG
 }
 
 sub select {
     confess 'Redis method "select" is prohibited for '.__PACKAGE__.' objects'
 }
 
-my $CBOR_MAGIC=$CBOR::XS::MAGIC;
-
 sub write {
-    my $redC=shift;
-    my $cb=pop(@_) if ref $_[$#_] eq 'CODE';
+    my $redC = shift;
+    
+    my $cb = pop(@_) if is_plain_coderef($_[$#_]);
     return unless @_;
-    my $method=(@_>2?'m':'').'set';
+    my $method = (@_>2 ? 'm' : '') . 'set';
+    my $encTag = $redC->encoder;
     $redC->$method(
-        (map { 
-            my ($k,$v)=(shift,shift); 
-            $k=>ref($v)
-                ? $CBOR_MAGIC.$cbor->encode($v)
+        ( map {
+            my ($k, $v) = (shift, shift);
+            $k => ref($v)
+                ? encodeByTag($encTag => $v)
                 : do { utf8::encode($v) if utf8::is_utf8($v); $v }
-        } 1..+(scalar(@_)>>1)),
-        $cb?($cb):()
+        } 1..+(scalar(@_)>>1) ),
+        $cb ? ($cb) : ()
     );
 }
 
-my $readCallback=<<'EOSUB';
+sub read {
+    my $redC = shift;
+    my $opts = is_plain_hashref($_[0]) ? do { $_=shift; %{$_}? $_ : {'ret_hash_ref'=>1} } : undef;
+    my $cb = pop(@_) if is_plain_coderef($_[$#_]);
+    return unless @_;
+    my $flRetHashRef = $opts->{'ret_hash_ref'};
+    my $k = [ map { is_plain_arrayref($_) ? @{$_} : $_ } @_ ];
+    my $method = ($#{$k} ? 'm' : '') . 'get';
+    my $retv;
+    $redC->$method( @{$k} => 
     sub {
         if ( defined $_[1] ) {
-            if ($cb) {
+            if ( $cb ) {
                 $cb->(@_)
             } else {
                 confess 'Redis error: '.$_[1]
             }
         }
-        my $cbor=CBOR::XS->new;
-        my $CBOR_MAGIC=$CBOR::XS::MAGIC;
         $retv=
-            $opts->{'ret_hash_ref'}
+            $flRetHashRef
             ? do {
                 my $c=0;
-                scalar({ map { $k->[$c++]=>%ALTCODE% } ref($_[0]) eq 'ARRAY'?@{$_[0]}:($_[0]) })
+                scalar({ map { $k->[$c++] => decodeByTag($_) } is_plain_arrayref($_[0]) ? @{$_[0]} : ($_[0]) })
               }
-            : [ map { %ALTCODE% } ref($_[0]) eq 'ARRAY'?@{$_[0]}:($_[0]) ];
+            : [ map decodeByTag($_), is_plain_arrayref($_[0]) ? @{$_[0]} : ($_[0]) ];
         $cb->( $retv ) if $cb;
-    }
-EOSUB
-my @altReadCode=map {$readCallback=~s/%ALTCODE%/$_/gre} (
-    <<'EOCODE',
-        ( defined($_) and length($_)>3 and substr($_,0,3) eq $CBOR_MAGIC )
-            ? $cbor->decode($_)
-            : defined($_)
-                ? do { utf8::decode($_); $_ }
-                : undef
-EOCODE
-        q{defined($_)?$cbor->decode($_):undef},
-);
-
-sub read {
-    my $redC=shift;
-    my $opts=ref($_[0]) eq 'HASH'?do { $_=shift; %{$_}?$_:{'ret_hash_ref'=>1} }:undef;
-    my $cb=pop(@_) if ref $_[$#_] eq 'CODE';
-    return unless @_;
-    my $k=[map {ref($_) eq 'ARRAY'?@{$_}:$_} @_];
-    my $method=($#{$k}?'m':'').'get';
-    my $retv;
-    $redC->$method(@{$k}, eval($altReadCode[$opts->{'only_cbor'}?1:0]) || confess 'EVAL ERROR: '.$@);
+    });
     return 1 if $cb;
     $redC->wait_all_responses;
     return $retv;
@@ -143,7 +141,7 @@ sub get_redc_by {
 }
 
 sub DESTROY {
-    delete $conoByName{delete($conoByRef{refaddr $_[0]})->{'name'}};
+    delete $conoByName{ delete($conoByRef{refaddr $_[0]})->{'name'} };
 }
 
 1;
