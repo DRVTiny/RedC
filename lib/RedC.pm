877d790c83502ec5ddab7a62f8cd1ae368f6e147
Andrey A. Konovalov <drvtiny@gmail.com>
2018-06-08 23:22:08 +0300

    You can switch to another db: select() method now works properly, new index() method

--- lib/RedC.pm
+++ lib/RedC.pm
@@ -4,6 +4,7 @@ use constant {
     RECONNECT_UP_TO	=>	 10,		# Reconnect up to 10 seconds
     RECONNECT_EVERY	=>	 100_000,	# Reconnect every 100 ms
     DFLT_ENCODER_TAG	=> 	'CB',		# Ecnode complex structures to CBOR by default
+    DFLT_REDIS_DB_N	=>	 0,
 };
 
 use strict;
@@ -13,9 +14,9 @@ binmode $_=>':utf8' for *STDOUT,*STDERR;
 use 5.16.1;
 
 use Tag::DeCoder;
-use Carp qw(confess);
-use Ref::Util qw(is_hashref is_plain_coderef is_plain_hashref is_plain_arrayref);
-use Scalar::Util qw(refaddr);
+use Carp		qw(confess);
+use Ref::Util 		qw(is_hashref is_plain_coderef is_plain_hashref is_plain_arrayref);
+use Scalar::Util 	qw(refaddr);
 
 use Exporter qw(import);
 our @EXPORT_OK = qw(get_redc_by);
@@ -24,54 +25,62 @@ my (%conoByName, %conoByRef);
 # By default, after connection failure try to reconnect every 100 ms up to 10 seconds
 my $redTestObj = Redis::Fast->new( 'reconnect' => RECONNECT_UP_TO, 'every' => RECONNECT_EVERY);
 sub new {
-    state $exclOptions={'on_connect'=>1, 'encoder'=>1};
-    my ($class, %options)=@_;
+    state $exclOptions = +{ map {$_=>1} qw(on_connect redc index name client_name encoder) };
+    my ($class, %options) = @_;
     
     my ($coName, $conIndex) = delete @options{qw/name index/};
-    $conIndex=0 unless defined $conIndex and $conIndex!~/[^\d]/;
+    $conIndex = DFLT_REDIS_DB_N unless defined $conIndex and $conIndex !~ /[^\d]/;
     $coName //= join('_' => 'redc', 'anon', 'pid'.$$, int(rand 65536));
-    confess qq(Cant initialize RedC: name '$coName' was already reserved as Redis connector name)
-        if $conoByName{$coName};
+    $conoByName{$coName} and confess sprintf(q<Cant initialize RedC: name '%s' was already reserved as a Redis connector name>, $coName);
     
-    if ($conIndex) {
-        my $nDB = databases($redTestObj);
-        confess sprintf('Redis database index #%s is out of configured bounds (min=0, max=%d)',$conIndex, $nDB-1)
-            if $conIndex>=$nDB;
+    if ( $conIndex ) {
+        my $nDB = databases( $redTestObj );
+        confess sprintf('Redis database index #%s is out of configured bounds (min=0, max=%d)',$conIndex, $nDB - 1)
+            if $conIndex >= $nDB;
     }
-    
-    my $redC = $class->SUPER::new(
+    my $redC;
+    @options{ qw/name index redc/ } = ( $coName, $conIndex, \$redC );
+    ($options{'client_name'} //= $coName . '__' . $$) =~ s%\s%_%g;
+    for ( $options{'encoder'} ) {
+        defined($_) && !ref($_) && length($_)
+            ? length($_)>2 && substr($_, 0, 1) eq '{' && substr($_, -1, 1) eq '}'
+                ? do { substr($_,0,1)=''; substr($_,1,1)=''; $_ }
+                : $_
+            : ($_ = DFLT_ENCODER_TAG)
+    }
+    my $opts = \%options;
+    $redC = $class->SUPER::new(
 # Hint: you can redefine "reconnection" settings in your %options passed to constructor
         'reconnect'	=>	RECONNECT_UP_TO,
         'every'		=>	RECONNECT_EVERY,
         'on_connect'	=>	sub {
-            my $self = shift;
-            $options{'on_connect'} and is_plain_coderef($options{'on_connect'}) and $options{'on_connect'}->($self);
-            $self->SUPER::select($conIndex) if $conIndex;
-            $self->SUPER::client_setname(
-                (
-                    (defined($options{'client_name'}) and !ref $options{'client_name'})
-                        ? $options{'client_name'}
-                        : "${coName}_$$"
-                ) =~ s%\s%_%gr
-            );
+            my $self = $_[0];
+            __call_this_if_coderef( $opts->{'on_connect'}, $self );
+            $self->SUPER::select( $opts->{'index'} );
+            $self->SUPER::client_setname( $opts->{'client_name'} //= ($opts->{'name'} . '__' . $$) =~ s%\s%_%gr );
         },
-        (map {$_=>$options{$_}} grep { !$exclOptions->{$_} } keys %options),
+        (map { $_ => $options{$_} } grep { !$exclOptions->{$_} } keys %options),
     );
-    $conoByRef{refaddr $redC} = $conoByName{$coName} = {
-        'name'	=>	$coName,
-        'index'	=>	$conIndex,
-        'redc'	=>	\$redC,
-        $options{'encoder'} ? ('encoder_tag'=>$options{'encoder'}) : ()
-    };
-    return $redC;
+    $conoByRef{refaddr $redC} = $conoByName{$coName} = $opts;
+    return $redC
 }
 
 sub encoder {
-    $_[1] ? $conoByRef{refaddr $_[0]}{'encoder_tag'} = $_[1] : $conoByRef{refaddr $_[0]}{'encoder_tag'} //= DFLT_ENCODER_TAG
+    $_[1] ? $conoByRef{refaddr $_[0]}{'encoder'} = $_[1] : $conoByRef{refaddr $_[0]}{'encoder'} //= DFLT_ENCODER_TAG
 }
 
 sub select {
-    confess 'Redis method "select" is prohibited for '.__PACKAGE__.' objects'
+    my ($self, $redisDbN) = @_;
+    my $p_ind = \$conoByRef{refaddr $self}{'index'};
+    return $self if ${$p_ind} == $redisDbN;
+    # Exception will be raised by the parent Redis::Fast class if something goes wrong, so we dont need to take care about select() result validation
+    $self->SUPER::select( $redisDbN );
+    ${$p_ind} = $redisDbN;
+    return $self
+}
+
+sub index {
+    defined($_[1]) ? $_[0]->select($_[1]) : $conoByRef{refaddr $_[0]}{'index'}
 }
 
 my %writeMethodsDiff = (
@@ -160,24 +169,27 @@ sub read {
 }
 
 sub databases {
-    my $redC=shift;
+    my $redC = shift;
     open my $fh, '<', $redC->info->{'config_file'};
     return ((local $/=<$fh>)=~m/(?:^|\n)\s*databases\s+(\d+)\s*(?:$|\n)/sm)[0];
 }
 
 sub get_redc_by {
+# get_redc_by is a class-wide method that is agnostic to the passed object instance
     shift if ref $_[0] eq __PACKAGE__;
-    my $byWhat=shift;
+    my $byWhat = shift;
     return if ref $byWhat or !defined($byWhat);
     given ($byWhat) {
-        return $conoByName{$byWhat} 
-            when 'name';
-        return [grep {$_->{'index'}==$byWhat} values %conoByName] 
-            when 'index';
+        $conoByName{$byWhat} when 'name';
+        [ grep {$_->{'index'} == $byWhat} values %conoByName ] when 'index';
         default {
-            confess 'No such connector object attribute: '.$byWhat
+            confess sprintf('No such connector object attribute <<%s>>', $byWhat)
         }
     }
+}
+
+sub __call_this_if_coderef {
+    defined($_[0]) and is_plain_coderef($_[0]) and $_[0]->( $#_ > 0 ? @_[1..$#_] : () )
 }
 
 sub DESTROY {
